0ad6aea87121dc70807bd3d6d36b0413767d069d
Carroth23 <wlsrb230@gmail.com>
2022-02-14 15:05:31 +0900

    멤버컨트롤러 및 서비스레이어 생성

---
+++ src/main/java/my/toy/dao/MemberDAO.java
@@ -0,0 +1,8 @@
+package my.toy.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+	
+}
---
+++ src/main/java/my/toy/service/MemberService.java
@@ -0,0 +1,8 @@
+package my.toy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	
+}
