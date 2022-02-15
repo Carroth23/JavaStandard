4f56f5344ebeebc12705fa474774da749bc48fa3
Carroth23 <wlsrb230@gmail.com>
2022-02-15 21:15:43 +0900

    컴포넌트스캔 확인

--- src/main/java/my/toy/controller/HomeController.java
+++ src/main/java/my/toy/controller/HomeController.java
@@ -6,12 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class HomeController {
 
 	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
+		System.out.println("이게 찍혀야 홈으로 온겁니다.");
+		System.out.println("이게 찍혀야 홈으로 온겁니다.");
 		return "Home";
 	}
 	
--- src/main/java/my/toy/controller/MemberController.java
+++ src/main/java/my/toy/controller/MemberController.java
@@ -1,5 +1,7 @@
 package my.toy.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,4 +14,14 @@ public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
+	
+	@Autowired
+	HttpSession session;
+	
+	@RequestMapping("login")
+	public String login(String id, String pw) {
+		session.setAttribute("id", pw);
+		System.out.println("들어온 id : " + id);
+		return "Home";
+	}
 }
