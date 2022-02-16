fecbc7059b7d331e78f85fd177c2b8f17e56e2c8
Carroth23 <wlsrb230@gmail.com>
2022-02-16 20:34:27 +0900

    우왕 별찍기

---
+++ boj/Proc/src/boj/Main.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+			int s = Integer.parseInt(br.readLine());
+			for(int i = 1; i <= s; i++) {
+				String beforeS = br.readLine();
+				int a = Integer.parseInt(beforeS.split(" ")[0]);
+				int b = Integer.parseInt(beforeS.split(" ")[1]);
+				bw.write("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+				bw.newLine();
+			}
+			bw.flush();
+		} catch(Exception e) {
+			
+		}
+	}
+}
