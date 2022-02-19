2f28318bbfe0fd27318376c8e6d00a7f4841a76f
Carroth23 <wlsrb230@gmail.com>
2022-02-19 23:20:57 +0900

    문자열 자르기

--- boj/Proc/src/boj/Main.java
+++ boj/Proc/src/boj/Main.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String a = sc.nextLine();
-		int b = a.charAt(0);
-		System.out.println(b);
+		int a = Integer.parseInt(sc.nextLine());
+		String b = sc.nextLine();
+		int c = 0;
+		for (int i = 0; i < a; i++) {
+			c += Integer.parseInt(b.substring(i, i+1));
+		}
+		System.out.println(c);
 		
 		}
 	}
