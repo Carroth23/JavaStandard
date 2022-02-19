7ddd47db0fbff4806707fc7ecc9fc378ae393f26
Carroth23 <wlsrb230@gmail.com>
2022-02-19 23:12:50 +0900

    아스키?

--- boj/Proc/src/boj/Main.java
+++ boj/Proc/src/boj/Main.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.close();
-		int nCopy = n;
-		int tmp = 0;
-		int i = 0;
-		while(true) {
-			
+		String a = sc.nextLine();
+		int b = a.charAt(0);
+		System.out.println(b);
+		
 		}
 	}
-}
