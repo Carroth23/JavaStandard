93c4622391d7914fa203185f4b48022c76ea60b8
Carroth23 <wlsrb230@gmail.com>
2022-03-18 14:49:32 +0900

    N: 열거형

---
+++ 자바의정석/src/열거형/열거형01.java
@@ -0,0 +1,41 @@
+package 열거형;
+
+public class 열거형01 {
+	// 열거형: 관련된 상수들을 같이 묶어 놓은것.(객체임)
+	enum Direction {EAST, SOUTH, WEST, NORTH};
+	enum Direction2 {EAST, SOUTH, WEST, NORTH};
+		// 열거형 이름   o,    1,    2,     3 자동초기화
+	
+	public static void main(String[] args) {
+//		if(Direction.EAST == Direction2.EAST) 타입을 먼저체크. 컴파일 에러
+		System.out.println(Direction.EAST);
+		Direction dir; //열거형 변수 선언
+		dir = Direction.NORTH; // dir에는 해당 열거형 상수만 들어올 수 있음
+		System.out.println(dir);
+		
+		// == 열거형 상수의 비교에 == 사용 가능
+		System.out.println(dir == Direction.NORTH);
+		System.out.println(Direction.NORTH == Direction.EAST);
+		
+		// >< 비교연산자는 불가능. compareTo로 사용.
+//		System.out.println(dir > Direction.EAST); 에러
+		System.out.println(dir.compareTo(Direction.EAST)); // NORTH=3 - EAST=0 = 3
+		
+		// 모든 열거형의 조상 Enum
+		
+		switch(dir) {
+		case EAST: // Direction.EAST라고 쓰면 안된다. 
+			System.out.println("EAST");
+			break;
+		case NORTH:
+			System.out.println("NORTH");
+			break;
+		}
+		
+		Direction[] dArr = Direction.values(); // 열거형의 모든 상수를 배열로 반환
+		for(Direction d : dArr) {
+			System.out.println(d.name() + ", " + d.ordinal()); // ordinal은 값이 아니라 순서
+		}
+	}
+
+}
---
+++ 자바의정석/src/열거형/열거형02.java
@@ -0,0 +1,19 @@
+package 열거형;
+
+public class 열거형02 {
+
+	// 열거형에 원하는 값(멤버) 넣기
+//	enum Direction{ EAST(10), SOUTH(5), WEST(10), NORTH(20/*, "하이" 두개넣기도 가능*/) }
+	enum Direction {
+		EAST(10), SOUTH(5), WEST(10), NORTH(20/* , "하이" 두개넣기도 가능 */); // ; 추가
+		// 원하는 값을 넣으려면 괄호에 값을 넣고, 지정된 값을 저장할 수 있는 변수와 생성자를 추가해줘야함.
+		private final int value; // private 이어야한다. 상수이므로 final붙여봄
+		Direction(int value){this.value = value;} // 생성자.(private이 붙어있다고 생각하면됨)
+		
+		public int getValue() {return value;} // 값 가져오는 메서드
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Direction.EAST.getValue());
+	}
+}
