f32c7c9d75e356c7aa9f3995ea25892d1912206f
Carroth23 <wlsrb230@gmail.com>
2022-04-05 18:26:37 +0900

    N: 람다식 예제

--- Toy/src/Toy.java
+++ Toy/src/Toy.java
@@ -1,28 +1,44 @@
 import java.sql.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
-
-@FunctionalInterface
-interface MyFunction {
-    void run(); // public abstract void run();
-}
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Toy {
-    static void execute(MyFunction f) { // 매개변수 타입이 펑셔날
-        f.run();
+    public static void main(String[] args) {
+        Supplier<Integer> s = () -> (int)(Math.random() * 100) + 1;
+        Consumer<Integer> c = i -> System.out.print(i + ", ");
+        Predicate<Integer> p = i -> i % 2 == 0;
+        Function<Integer, Integer> f = i -> i / 10 * 10;
+
+        List<Integer> list = new ArrayList<>();
+        //////////////////////////// 써야댐
     }
 
-    static MyFunction getMyFunction() {
-        return () -> System.out.println("f3.run()");
+    static <T> List<T> doSomething(Function<T, T> f, List<T> list) {
+        List<T> newList = new ArrayList<T>(list.size());
+
+        for (T i : list) {
+            newList.add(f.apply(i));
+        }
+        return newList;
     }
 
-    public static void main(String[] args) throws Exception {
-        // 람다로 구현
-        MyFunction f = () -> System.out.println("f1.run()");
-
-        getMyFunction().run();
-        execute(() -> System.out.println("execute"));
-
+    static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
+        System.out.print("[");
+        for(T i : list){
+            if(p.test(i)){
+                c.accept(i);
+            }
+        }
+        System.out.println("]");
     }
 
+    static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add(s.get());
+        }
+    }
 }
