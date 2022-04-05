ff8c650f913b0a5342d80111b2e9acb1173fe0be
Carroth23 <wlsrb230@gmail.com>
2022-04-05 23:28:04 +0900

    forEach

--- Toy/src/Toy.java
+++ Toy/src/Toy.java
@@ -1,6 +1,8 @@
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -8,37 +10,29 @@ import java.util.function.Supplier;
 
 public class Toy {
     public static void main(String[] args) {
-        Supplier<Integer> s = () -> (int)(Math.random() * 100) + 1;
-        Consumer<Integer> c = i -> System.out.print(i + ", ");
-        Predicate<Integer> p = i -> i % 2 == 0;
-        Function<Integer, Integer> f = i -> i / 10 * 10;
 
-        List<Integer> list = new ArrayList<>();
-        //////////////////////////// 써야댐
-    }
-
-    static <T> List<T> doSomething(Function<T, T> f, List<T> list) {
-        List<T> newList = new ArrayList<T>(list.size());
-
-        for (T i : list) {
-            newList.add(f.apply(i));
-        }
-        return newList;
-    }
-
-    static <T> void printEvenNum(Predicate<T> p, Consumer<T> c, List<T> list) {
-        System.out.print("[");
-        for(T i : list){
-            if(p.test(i)){
-                c.accept(i);
-            }
-        }
-        System.out.println("]");
-    }
-
-    static <T> void makeRandomList(Supplier<T> s, List<T> list) {
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            list.add(s.get());
+            list.add(i);
         }
+
+        // list의 모든 요소 출력
+        list.forEach(i -> System.out.print(i + ","));
+        System.out.println();
+
+        // list에서 2 또는 3의 배수를 제거한다.
+        list.removeIf(x -> x % 2 == 0 || x % 3 ==0);
+        System.out.println(list);
+
+        list.replaceAll(i -> i * 10);
+        list.forEach(i -> System.out.print(i + ", "));
+        System.out.println();
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        map.put("4", "4");
+
+        map.forEach((x, y) -> System.out.println("key = " + x + ", val = " + y));
     }
 }
