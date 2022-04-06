f83f9252d8c221dafe0f624c5e02abf850c07401
Carroth23 <wlsrb230@gmail.com>
2022-04-06 12:47:45 +0900

    N: Optiona 복습

--- Toy/src/Toy.java
+++ Toy/src/Toy.java
@@ -1,25 +1,24 @@
 import java.io.File;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.stream.Stream;
 
 
 public class Toy {
     public static void main(String[] args) {
-        File[] fileArr = {new File("Ex1.java"), new File("Ex1.bak"),
-        new File("Ex2.java"), new File("Ex1"), new File("Ex1.txt")};
+        Optional<String> optStr = Optional.of("abcde");
+        Optional<Integer> optInt = optStr.map(String::length);
 
-        Stream<File> fileStream = Stream.of(fileArr);
+        int result1 = Optional.of("123")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).orElse(0);
 
-        //map()으로 Stream<File>을 Stream으로 변환
-        Stream<String> stringStream = fileStream.map(f -> f.getName());
-        stringStream.forEach(System.out::println);
+        int result2 = Optional.of("")
+                .filter(x -> x.length() > 0)
+                .map(Integer::parseInt).orElse(-1);
 
-        fileStream = Stream.of(fileArr);
-        fileStream.map(f -> f.getName()).filter(s -> s.indexOf('.') != -1)
-                .map(s -> s.substring(s.indexOf('.') + 1))
-                .peek(s -> System.out.printf("filename=%s%n", s))
-                .map(s -> s.toUpperCase(Locale.ROOT))
-                .distinct()
-                .forEach(System.out::print);
+        Optional.of("456").map(Integer::parseInt)
+                .ifPresent(System.out::println);
     }
 }
