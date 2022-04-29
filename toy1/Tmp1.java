a043071bcb49b2566565e8cb3a55f99b79b9d020
Carroth23 <wlsrb230@gmail.com>
2022-04-29 12:01:37 +0900

    modify: 2차원 배열 복습

--- toy1/Tmp1.java
+++ toy1/Tmp1.java
@@ -3,8 +3,14 @@ package toy1;
 public class Tmp1 {
 
 	public static void main(String[] args) {
-		System.out.println(Math.round(4.534 * 100) / 100.0);
-		System.out.println(3 / 0);
+		int[][] arr = new int[9][9];
+		for(int i = 0; i <= 8; i++) {
+			for(int j = 0; j <= 8; j++) {
+				arr[i][j] = j+1;
+			}
+		}
+		
+		System.out.println(arr[3][3] * arr[3][4]);
 	}
 
 }
