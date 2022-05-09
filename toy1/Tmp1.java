793d946779b87c3cff088af7b8b0b3ab9bac7b3e
Carroth23 <wlsrb230@gmail.com>
2022-05-09 12:32:27 +0900

    Feat: 이클립스 기본단축키
    이클립스 기본단축키와 이클립스에서 코드어시스턴스 사용하는법

--- toy1/Tmp1.java
+++ toy1/Tmp1.java
@@ -3,14 +3,7 @@ package toy1;
 public class Tmp1 {
 
 	public static void main(String[] args) {
-		int i;
-		int j = 0;
-		for(i = 2; i <= 4; i++) {
-			for (j = 5; j <= 7; j++) {
-				
-			}
-		}
-		System.out.printf("%d x %d = 2%d", j, i, (i * j));
+		System.out.print("Hello World!!");
 	}
 
 }
---
+++ 자바기본/이클립스기본단축키.java
@@ -0,0 +1,20 @@
+package 자바기본;
+
+public class 이클립스기본단축키 {
+	public static void main(String[] args) {
+		
+		// ctrl + shift + L 	단축키 전체 목록 보기
+		// ctrl + +, - 			폰트크기 변경
+		// ctrl + D 			한줄 삭제
+		// ctrl + alt + down,up	행단위 복사
+		// alt + shift + A		멀티컬럼 편집
+		// alt + up, down		행단위 이동
+		// tab, shift + tab		들여쓰기, 내어쓰기
+		// ctrl + I				자동 들여쓰기
+		// ctrl + /				한줄 주석(토글)
+		// ctrl + space			자동완성
+		// Preference -> Java -> Editor -> Content Assist Auto Activation
+		// 트리거에 .abcdefghijk..등을 입력해두면 코드 칠때마다 추천단어 뜸(편리)
+		
+	}
+}
