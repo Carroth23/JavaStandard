1e4bb4ac950e26875240df436b4c2a7bde94d82b
Carroth23 <wlsrb230@gmail.com>
2022-05-09 13:53:48 +0900

    Feat: printf 실습 println말고 f도 사용

--- toy1/Tmp1.java
+++ toy1/Tmp1.java
@@ -3,7 +3,13 @@ package toy1;
 public class Tmp1 {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!!");
+		float qq = 123.456789f;
+		System.out.printf("%d%n", 10);
+		System.out.println(10/3.0);
+		
+		String url = "qwertyuiop";
+		System.out.printf("%.3s%n", url);
+		System.out.printf("%f%n", qq);
 	}
 
 }
--- 변수_상수_리터럴/변상리.java
+++ 변수_상수_리터럴/변상리.java
@@ -3,14 +3,28 @@ package 변수_상수_리터럴;
 public class 변상리 {
 
 	public static void main(String[] args) {
+		// 변수: 하나의 값을 저장하기 위한 메모리 공간(RAM)
+		// 변수의 선언 이유: 값을 저장할 "공간"을 마련하기 위해
+		int q = 0, w = 1; // 타입이 같으면 ,로 구분
+		
+		// 변수의 종류: 클래스변수, 인스턴스변수, 지역변수(지역변수는 초기화 필수)
+		
 		// 기본형 크기
 		// 1byte: boolean, byte
 		// 2byte: char, short
 		// 4byte: int, float
 		// 8byte: long, double
 		
+		// 기본형 타입은 실제 값을 저장
+		// 참조형 타입은 메모리 주소를 저장
+		
 		int num = 100_000_000;
 		System.out.println("JDK1.7부터 정수형에 _로 구분 가능" + num);
+		
+		String s = ""; // 빈 문자열
+//		char ch = ''; //에러
+		// "" + 7 + 7 = "77"
+		// 7 + 7 + "" = "14"
 		
 		// 변수 값 바꾸기
 		int x = 10;
@@ -18,14 +32,16 @@ public class 변상리 {
 		int tmp;
 		tmp = x;
 		x = y;
-		y = x;
+		y = tmp;
 		System.out.println("x = " + x + ", y = " + y);
 		
 		// printf출력
 		double d = 12.34567;
-		System.out.printf("age : %d%n", x);
+		System.out.printf("age : %3d%n", x); // 3칸
+		System.out.printf("%.2s%n", "qwertyuiop"); // 2개만출력
 		System.out.printf("double : %.1f%n", d);
 		System.out.printf("double : %.2f%n", d); // 반올림됨
+		System.out.printf("%s%n", Integer.toBinaryString(15)); // 2진 문자열
 		
 		// 오버플로우
 		short beforeOver = 32767;
@@ -37,11 +53,15 @@ public class 변상리 {
 		char c = '3';
 		int c2 = c - '0';
 		
-		//상수는 한번 값을 저장하면 다른값으로 변경 불가.
+		// 상수는 한번 값을 저장하면 다른값으로 변경 불가.(값을 변경 할 수 없는 변수)
 		final int MAX_SPEED = 10; //상수는 대문자로 하고 단어의 구분은_로 한다.
 		
-		//리터럴은 그 자체로 값을 의미함.
+		// 리터럴은 그 자체로 값을 의미함.
 		int year = 2014; //리터럴 = 2014
+		
+		// 접미사
+		// L과 f만 주의.
+		// 실수형 float은 f, double는 b를 붙여야 하지만 타입이 두개이다보니 d는 생략 가능
 	}
 
 }
