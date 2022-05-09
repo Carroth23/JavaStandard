f19bbb0fd0f231069e39b4e15162ced7f95bf361
Carroth23 <wlsrb230@gmail.com>
2022-05-09 16:35:58 +0900

    Feat: 연산자 복습
    연산자챕터를 복습하고 복합대입연산자등을 삽입

--- toy1/Tmp1.java
+++ toy1/Tmp1.java
@@ -3,13 +3,8 @@ package toy1;
 public class Tmp1 {
 
 	public static void main(String[] args) {
-		float qq = 123.456789f;
-		System.out.printf("%d%n", 10);
-		System.out.println(10/3.0);
-		
-		String url = "qwertyuiop";
-		System.out.printf("%.3s%n", url);
-		System.out.printf("%f%n", qq);
+		byte q = 127;
+		System.out.println(++q);
 	}
 
 }
--- 연산자/연산자01.java
+++ 연산자/연산자01.java
@@ -6,10 +6,13 @@ public class 연산자01 {
 		// 삼항연산자
 		System.out.println("삼항 연산자 : " + (3 > 5 ? true : false));
 		
-		// 전위형, 후위형
+		// 대입연산자는 오른쪽에서 왼쪽으로
+		
+		// 증감연산자 전위형, 후위형
 		int i = 5, j =5;
 		System.out.println("전위형 : " + ++i);
 		System.out.println("후위형 : " + j++ + ", 후위 후 j : " + j);
+		// 증감연산자는 일반 산술 변환에 의한 자동 형변환이 발생하지 않으며 연산 결과의 타입은 피연산자의 타입과 일치함
 		
 		// 형변환 연산자
 		double d = 85.4;
@@ -26,12 +29,15 @@ public class 연산자01 {
 		System.out.printf("%d / %d = %f%n", a, c, (float)a / c);
 		
 		// 산술변환
-		// 이항 연산자는 두 피연산자의 타입이 일치해야 하므로 형변환 연산자로 타입을 일치시킨 후 연산.
+		// 이항 연산자는 두 피연산자의 타입이 일치해야 하므로 형변환 연산자로 (큰쪽으로)타입을 일치시킨 후 연산.
 		// long + int -> long + long = long
 		// float + in -> float + float = float
 		// 피연산자의 타입이 int보다 작은 타입이면 int로 변환
 		// byte + short -> int + int = int
 		// char + short -> int + int = int
+		int aa = 1_000_000;
+		int bb = 2_000_000;
+		long cc = (long)aa * bb; // int * int = int이기 때문에 오버플로우 발생(형변환 사용)
 		
 		// Math.round()
 		float pi = 3.141592f;
@@ -40,7 +46,7 @@ public class 연산자01 {
 		// 나머지 연산자 %
 		System.out.println("부호는 무시된다 " + -10 % 8);
 		
-		// 비교 (기본형은 값비교)
+		// 비교 (기본형은 값비교, 참조형은 객체의 주소값을 비교)
 		String str1 = "하이";
 		String str2 = "하이";
 		String str3 = new String("하이");
@@ -48,10 +54,27 @@ public class 연산자01 {
 		System.out.println(str1 == str3);
 		System.out.println(str1.equals(str3));
 		
+		// 논리 연산지 && ||
+		// ||(OR)
+		// &&(AND)
+		// &&가 ||보다 우선순위가 높다. ()로 순서정해야함
+		// 문자 ch는 숫자('0' ~ '9')이다.
+		// '0' <= ch && ch <= '9' (유니코드로 변환되어 계산.)
+		
 		// 숏컷연산
 		int z = 10;
 		int y = 20;
 		System.out.println((++z == 11 || ++y == 30) + "z : " + z + ", y : " + y);
+		
+		// 논리 부정 연산자 !
+		boolean g = true;
+		boolean g1 = !g; // false
+		boolean g2 = !!g; // true
+		System.out.println("g1 = " + g1 + ", g2 = " + g2);
+		
+		// 복합 대입 연산자
+		// i = i + 3; -> i += 3;
+		// i = i * (10 + j); -> i *= 10 + j;
 	}
 
 }
